a94755259fa7faf5d7db612f2df42545e6143bae
IHU <2022ihu@gmail.com>
2023-01-06 21:32:50 +0900

    avatar test

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Model from "./components/Ihu";
 import ModelMove from "./components/Ihu-move";
+import "./App.css";
 
 function App() {
   return (
-    <Canvas
-      camera={{ position: [2, 0, 12.25], fov: 15 }}
-      style={{
-        backgroundColor: "#111a21",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <ambientLight intensity={1.25} />
-      <ambientLight intensity={0.1} />
-      <directionalLight intensity={0.4} />
-      <ModelMove position={[0.025, -0.9, 0]} />
-      <OrbitControls />
-    </Canvas>
+    <div>
+      <div className="App">
+        <h1>avatar test</h1>
+      </div>
+      <Canvas
+        camera={{ position: [2, 0, 12.25], fov: 15 }}
+        style={{
+          backgroundColor: "#111a21",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <ambientLight intensity={1.25} />
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={0.4} />
+        <ModelMove position={[0.025, -0.9, 0]} />
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 }
 
